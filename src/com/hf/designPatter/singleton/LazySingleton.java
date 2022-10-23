2393526e2789cf1805b8bfdb06d899d44c9e62f1
xiehongfei <hoongfei@qq.com>
2022-10-23 20:33:12 +0800

    singleton design pattern

--- src/com/hf/designPatter/singleton/LazySingleton.java
+++ src/com/hf/designPatter/singleton/LazySingleton.java
@@ -2,12 +2,12 @@ package com.hf.designPatter.singleton;
 
 /**
  * @author xiehongfei
- * @description 单例模式-懒汉模式(按需加载)
+ * @description 单例模式-懒汉模式(按需加载,双重加锁)
  * @date 2022/10/23 12:53
  */
 public class LazySingleton {
 
-    private static LazySingleton lazySingleton = null;
+    private static volatile LazySingleton lazySingleton = null;
 
     private LazySingleton() {
     }
@@ -16,7 +16,7 @@ public class LazySingleton {
         if (lazySingleton == null) {
             synchronized (LazySingleton.class) {
                 if (lazySingleton == null) {
-                    return new LazySingleton();
+                    lazySingleton = new LazySingleton();
                 }
             }
         }
---
+++ src/com/hf/designPatter/singleton/Test.java
@@ -0,0 +1,38 @@
+package com.hf.designPatter.singleton;
+
+/**
+ * @author xiehongfei
+ * @description
+ * @date 2022/10/23 18:16
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        // 饿汉式
+        HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+        HungrySingleton hungrySingleton2 = HungrySingleton.getInstance();
+        System.out.println(hungrySingleton.hashCode());
+        System.out.println(hungrySingleton2.hashCode());
+        System.out.println(hungrySingleton == hungrySingleton2);
+
+        // 懒汉式
+        LazySingleton lazySingleton = LazySingleton.getInstance();
+        LazySingleton lazySingleton2 = LazySingleton.getInstance();
+        System.out.println(lazySingleton.hashCode());
+        System.out.println(lazySingleton2.hashCode());
+        System.out.println(lazySingleton == lazySingleton2);
+
+        // 静态内部类式
+        StaticInnerClassSingleton staticInnerClassSingleton1 = StaticInnerClassSingleton.getInstance();
+        StaticInnerClassSingleton staticInnerClassSingleton2 = StaticInnerClassSingleton.getInstance();
+        System.out.println(staticInnerClassSingleton1.hashCode());
+        System.out.println(staticInnerClassSingleton2.hashCode());
+        System.out.println(staticInnerClassSingleton1 == staticInnerClassSingleton2);
+
+        EnumSingleton instance1 = EnumSingleton.INSTANCE;
+        EnumSingleton instance2 = EnumSingleton.INSTANCE;
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+        System.out.println(instance1 == instance2);
+    }
+}
