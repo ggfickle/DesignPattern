12ad3aceea08d256a5706ebf3afaa1f345d779dd
xiehongfei <hoongfei@qq.com>
2022-10-24 21:51:35 +0800

    strategy design pattern

---
+++ src/com/hf/designPatter/strategy/UserDiscountStrategyImpl.java
@@ -0,0 +1,17 @@
+package com.hf.designPatter.strategy;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * @author xiehongfei
+ * @description 普通会员策略
+ * @date 2022/10/24 21:36
+ */
+public class UserDiscountStrategyImpl implements DiscountStrategy{
+    @Override
+    public BigDecimal getDiscount(BigDecimal total) {
+        // 普通会员打九折
+        return total.multiply(new BigDecimal("0.10")).setScale(2, RoundingMode.HALF_UP);
+    }
+}
