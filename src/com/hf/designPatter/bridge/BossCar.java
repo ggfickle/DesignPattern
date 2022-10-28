667a0a2163dfcad3c2ae3c2126d1b3f9f231106d
yongen <yongen@zugeliang.com>
2022-10-28 21:33:14 +0800

    bridge&Chain pattern commit

---
+++ src/com/hf/designPatter/bridge/BossCar.java
@@ -0,0 +1,14 @@
+package com.hf.designPatter.bridge;
+
+public class BossCar extends Car{
+
+    public BossCar(Engine engine) {
+        super(engine);
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("boss car drive");
+        this.engine.start();
+    }
+}
