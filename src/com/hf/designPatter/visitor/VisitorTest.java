bb9fc94c12bc0fb100d16782c302a69e82916c94
yongen <yongen@zugeliang.com>
2022-10-30 17:40:18 +0800

    Visitor pattern commit

---
+++ src/com/hf/designPatter/visitor/VisitorTest.java
@@ -0,0 +1,15 @@
+package com.hf.designPatter.visitor;
+
+import java.io.File;
+
+public class VisitorTest {
+
+    /**
+     * 访问者模式
+     * @param args
+     */
+    public static void main(String[] args) {
+        FileStructure fs = new FileStructure(new File("."));
+        fs.handle(new JavaFileVisitor());
+    }
+}
